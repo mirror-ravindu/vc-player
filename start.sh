ee5e5aeaa2f67505526422d021c04181b418d15d
mirror-ravindu <87862291+mirror-ravindu@users.noreply.github.com>
2021-08-26 02:41:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/kusalCY/MusicPlayer.git /MusicPlayer
+git clone https://github.com/mirror-ravindu/vc-player.git /MusicPlayer
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
